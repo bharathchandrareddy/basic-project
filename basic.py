0cbc6923c63af4bf3606390ab3740293270a6afb
bharathchandrareddy <51413287+bharathchandrareddy@users.noreply.github.com>
2022-08-07 16:50:08 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Bharath Chandra "+ Fore.YELLOW+ Back.BLUE+"I am your Self Buddy")
+print(Back.CYAN+"Hi My name is Bharath Chandra")
+print(Fore.RED + Back.GREEN+ "Hi My name is Bharath chandra")
